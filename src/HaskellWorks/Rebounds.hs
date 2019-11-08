3a0c73aae8cb715534f93693aa6c6347f96e66ba
John Ky <newhoggy@gmail.com>
2019-11-08 13:37:04 +1100

    Megaparsec

--- src/HaskellWorks/Rebounds.hs
+++ src/HaskellWorks/Rebounds.hs
@@ -1,2 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module HaskellWorks.Rebounds
-  () where
+  ( lineComment
+  , scn
+  , sc
+  , lexeme
+  , pItem
+  ) where
+
+import Control.Monad        (void)
+import Data.Text            (Text)
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+import qualified Data.Text                  as T
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void Text
+
+lineComment :: Parser ()
+lineComment = L.skipLineComment "#"
+
+scn :: Parser ()
+scn = L.space space1 lineComment empty
+
+sc :: Parser ()
+sc = L.space (void $ some (char ' ' <|> char '\t')) lineComment empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+pItem :: Parser String
+pItem = lexeme (some (alphaNumChar <|> char '-')) <?> "list item"
+
+keyword :: Text -> Parser ()
+keyword = void . lexeme . string
+
+common :: Parser ()
+common = keyword "common"
+
+commonName :: Parser Text
+commonName = lexeme (fmap T.pack go) <?> "variable"
+  where go :: Parser String
+        go = ((:) <$> letterChar <*> many alphaNumChar)
